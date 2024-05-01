d2cef16650b73548bc9209f2a821dd730925d110
Azazelle Guice <azazelleguice@outlook.com>
2024-05-01 01:40:23 +0000

    Number of pi CLI

---
+++ num_of_pi.py
@@ -0,0 +1,28 @@
+import argparse, decimal
+
+def binary_split(a, b):
+    if b == a + 1:
+        Pab = -(6*a - 5)*(2*a - 1)*(6*a - 1)
+        Qab = 10939058860032000 * a**3
+        Rab = Pab * (545140134*a + 13591409)
+    else:
+        m = (a + b) // 2
+        Pam, Qam, Ram = binary_split(a, m)
+        Pmb, Qmb, Rmb = binary_split(m, b)
+        
+        Pab = Pam * Pmb
+        Qab = Qam * Qmb
+        Rab = Qmb * Ram + Pam * Rmb
+    return Pab, Qab, Rab
+
+def main(n):
+    """Chudnovsky algorithm."""
+    P1n, Q1n, R1n = binary_split(1, n)
+    return (426880 * decimal.Decimal(10005).sqrt() * Q1n) / (13591409*Q1n + R1n)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("number", type=int)
+    args = parser.parse_args()
+    decimal.getcontext().prec = args.number
+    print("   ", f"{main(1000):.{args.number - 1}f}")
